c70fb5cf979532b1feb14c21fc6cdce565e248eb
Christopher Bergman <cb.christopherbergman@gmail.com>
2021-04-21 14:42:11 +0200

    navbar starting

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,5 +1,14 @@
 import React from 'react';
-import { Nav, NavbarContainer, NavLogo } from './NavBarElements';
+import { FaBars } from 'react-icons/fa';
+import {
+	Nav,
+	NavbarContainer,
+	NavLogo,
+	MobileIcon,
+	NavMenu,
+	NavItem,
+	NavLinks,
+} from './NavBarElements';
 
 export const Navbar = () => {
 	return (
@@ -7,6 +16,17 @@ export const Navbar = () => {
 			<Nav>
 				<NavbarContainer>
 					<NavLogo to='/'>Arsadi</NavLogo>
+					<MobileIcon>
+						<FaBars />
+					</MobileIcon>
+					<NavMenu>
+						<NavItem>
+							<NavLinks to='about'>about</NavLinks>
+							<NavLinks to='contact'>Contact Form</NavLinks>
+							<NavLinks to='about'>about</NavLinks>
+							<NavLinks to='about'>about</NavLinks>
+						</NavItem>
+					</NavMenu>
 				</NavbarContainer>
 			</Nav>
 		</>
